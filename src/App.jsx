577891c0ab6a71dac1e4f02e0ac23c620ed527bf
zhalice2011 <zhalice2012@gmail.com>
2019-10-20 13:43:01 +0800

    init project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class App extends Component {
+
+    render() {
+        return (
+	        <div className="app">
+                <h1>从零开发口袋豆瓣</h1>
+	        </div>
+        );
+    }
+}
+
+module.exports = App;
+
+
