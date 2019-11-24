433d1ee4ed4bfb41590f810baeaf14319ba44a1b
zhalice2011 <zhalice2012@gmail.com>
2019-11-24 22:36:37 +0800

    add hooks

--- src/components/common/SearchBox.js
+++ src/components/common/SearchBox.js
@@ -1,40 +1,26 @@
-import React, { Component } from 'react';
+import React, { useState, Component } from 'react';
 import PropTypes from 'prop-types'
 import './SearchBox.css'
 
-class SearchBox extends Component {
-    static propTypes = {
-        onChange: PropTypes.func,
-    }
-    constructor (props) {
-        super(props)
-        this.state = {
-            search: ''
-        }
-    }
-    onChange = (e) => {
-        this.setState({ search: e.target.value })
-    }
-    applyChange = () => {
-        const { onChange } = this.props
-        onChange && onChange(this.state.search)
+function SearchBox ({ onChange }) {
+    const [ search, setSearch ] = useState('')
+
+    const applyChange = () => {
+        onChange && onChange(search)
     }
 
-    render() {
-        const { search } = this.state
-        return (
-	        <div className="row-flex search-container">
-                <input
-                    value={search}
-                    className="search-input"
-                    onChange={this.onChange}
-                />
-                <div className="search-btn" onClick={this.applyChange}>搜索</div>
-            </div>
-        );
-    }
+    return (
+        <div className="row-flex search-container">
+            <input
+                value={search}
+                className="search-input"
+                onChange={(e) => setSearch(e.target.value)}
+            />
+            <div className="search-btn" onClick={applyChange}>搜索</div>
+        </div>
+    )
 }
 
-module.exports = SearchBox
+export default SearchBox
 
 
